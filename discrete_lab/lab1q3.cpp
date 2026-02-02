2fe10994acae70fd1bac276a84c86861542ebc75
ryuu-Ken <kushal.magar19.km@gmail.com>
2026-02-02 11:58:43 +0545

    2nd ques done

--- discrete_lab/lab1q3.cpp
+++ discrete_lab/lab1q3.cpp
@@ -1,3 +1,49 @@
 #include<iostream>
 using namespace std;
 
+class student{
+    string name;
+    int roll;
+    int marks;
+
+    public:
+    void get_data(){
+        cout<<"Enter the name of the student: ";
+        getline(cin,name);
+        cout<<"Enter the roll no: ";
+        cin>>roll;
+        cout<<"Enter the marks: ";
+        cin>>marks;
+
+            if (marks > 90){
+                cout<<"Grade: A+";
+            }
+        
+            else if (marks >=80 && marks < 90){
+                cout<<"Grade: A";
+            }
+
+            else if (marks >=70 && marks <80){
+                cout<<"Grade: B+";
+            }
+
+            else if (marks >=60 && marks <70){
+                cout<<"Grade: B"; 
+            }
+
+            else if (marks >=50 && marks <60){
+                cout<<"Grade: C";
+            }
+    
+        else{
+        cout<<"Fail";
+        }
+    }
+
+};
+
+int main(){
+    student s;
+    s.get_data();
+    return 0;
+}
